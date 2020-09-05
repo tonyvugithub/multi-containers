76e914a1035e92fa65cb2df93d0cccb1ec00eb0f
tonyvugithub <55034244+tonyvugithub@users.noreply.github.com>
2020-09-05 18:30:57 -0400

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,6 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
